cdad4d158ed6b4f4b678750654cdb052bd9d8895
nocosharold <nocosharold@gmail.com>
2021-08-31 10:19:47 +0800

    Random Villains Every Fight

---
+++ assets/js/character.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        name: "Goodwin Walsh",
+        character_type: "hero",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Goodwin_Walsh_nA0CkzBol.png?updatedAt=1629869913685"
+    },
+    {
+        id: 2,
+        name: "Wilfrid Poole",
+        character_type: "hero",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Wilfrid_Poole_Yd_lWzL2e.png?updatedAt=1629869918201"
+    },
+    {
+        id: 3,
+        name: "Matt Swanson",
+        character_type: "hero",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Matt_Swanson_r7xuJWwLi.png?updatedAt=1629869915567"
+    },
+    {
+        id: 4,
+        name: "Spike Fennimore",
+        character_type: "villain",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Spike_Fennimore_vz9s5Q8WZ.png?updatedAt=1629869917356"
+    },
+    {
+        id: 5,
+        name: "Edie Miles",
+        character_type: "villain",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Edie_Miles_TWZagGIek.png?updatedAt=1629869912133"
+    },
+    {
+        id: 6,
+        name: "Edmond Buckley",
+        character_type: "villain",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Edmond_Buckley_uaBQ_GRNe.png?updatedAt=1629869912713"
+    },
+    {
+        id: 7,
+        name: "John Romero",
+        character_type: "villain",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/John_Romero_fxc7HyyVf.png?updatedAt=1629869914588"
+    },
+    {
+        id: 8,
+        name: "Samuel Vasquez",
+        character_type: "villain",
+        img: "https://ik.imagekit.io/1cqtok8fv7n/Hampas_Pang_Gas/full/Samuel_Vasquez_2EuMlCeZ8.png?updatedAt=1629869916457"
+    },
+];
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,69 @@
+import character_datas from "./character.js";
+
 $(document).ready(function(){
+    loadHeroCharacters();
+    loadVillainCharacters();
+
     $("#match_result").hide();
     $("body")
         .on("click", ".recruit_hero_button", switchScreen)
         .on("click", ".fight_button", generateMatchResult)
-        .on("click", "#close_modal_button", function(){
-            $(".modal_wrap").fadeOut().hide();
-            $(".modal_content h2").text("WAITING FIGHT RESULT...");
-        });
+        .on("click", "#close_modal_button", closeModal);
 });
+
+/**
+*   DOCU: This function is used to close modal
+*   Last updated at: August 31, 2021
+*   @author Harold
+*/
+function closeModal(){
+    $(".modal_wrap").fadeOut().hide();
+    $(".modal_content h2").text("WAITING FIGHT RESULT...");
+}
+
+/**
+*   DOCU: This function is used to load hero characters
+*   Last updated at: August 31, 2021
+*   @author Harold
+*/
+function loadHeroCharacters(){
+    let hero_template = ``;
+    
+    for(let character_index = 0; character_index < character_datas.length; character_index++){
+        if(character_datas[character_index].character_type === "hero"){
+            hero_template += `<div class="character" data-character_id=${character_datas[character_index].id}>`;
+            hero_template += ` <img src=${ character_datas[character_index].img }>`;
+            hero_template += ` <h3>${ character_datas[character_index].name }</h3>`;
+            hero_template += ` <button type="button" class="recruit_hero_button">RECRUIT</button>`;
+            hero_template += `</div>`;
+        }
+    }
+
+    hero_template += `<h2>RECRUIT <span>A HERO</span></h2>`;
+    $("#recruit_screen").html(hero_template);
+}
+
+/**
+*   DOCU: This function is used to load villain characters
+*   Last updated at: August 31, 2021
+*   @author Harold
+*/
+function loadVillainCharacters(){
+    let villain_template = ``;
+    let get_villains = character_datas.filter((character)=> character.character_type === "villain");
+    let random_data = get_villains[Math.floor(Math.random() * get_villains.length)];
+    let selected_villains = get_villains.filter((villain) => villain.id != random_data.id);
+
+    for(let villain_index = 0; villain_index < selected_villains.length; villain_index++){
+        villain_template += `<li class="character">`;
+        villain_template += `   <img src=${ selected_villains[villain_index].img }>`;
+        villain_template += `   <h3>${ selected_villains[villain_index].name }</h3>`;
+        villain_template += `   <button type="button" class="fight_button">FIGHT!</button>`;
+        villain_template += `</li>`
+    }
+
+    $(".villains").html(villain_template);
+}
 
 /**
 *   DOCU: This function is used to recruit a hero then switch to fight screen
@@ -16,14 +72,13 @@ $(document).ready(function(){
 *   @author Harold
 */
 function switchScreen(){
+    let character_id = $(this).parent().data("character_id");
+    let find_hero = character_datas.filter((character) => character.id === character_id);
+    const {img, name} =  find_hero[0];
+
     /* Recruit Hero */
-    let hero_name = $(this).parent().find('h3').text();
-    let hero_image = $(this).parent().find('img').attr('src');
-    let selected_character_template = ``
-    
-    selected_character_template += `<img src=${ hero_image }>`;
-    selected_character_template += `<h3>${ hero_name }</h3>`;
-    $("#selected_hero").html(selected_character_template);
+    $("#selected_hero img").attr("src",img);
+    $("#selected_hero h3").text(name);
 
     /* Switch Screens */
     $("#fight_screen").show();
@@ -52,5 +107,6 @@ function generateMatchResult(){
         $("#close_modal_button").addClass("show");
     }, 3000 );
 
+    loadVillainCharacters();
     $("#close_modal_button").removeClass("show");
 }
